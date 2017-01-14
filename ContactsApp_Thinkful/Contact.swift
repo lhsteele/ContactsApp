49c3dbaac0c8971c4cce328af019c3f06960e764
Lisa Steele <lisasteele@Lisas-Mac-mini.local>
2017-01-13 20:49:10 -0800

    Contacts App with list of contacts and a Detail View Controller to show each contact's details.

---
+++ ContactsApp_Thinkful/Contact.swift
@@ -0,0 +1,20 @@
+//
+//  Contact.swift
+//  ContactsApp_Thinkful
+//
+//  Created by Lisa Steele on 1/13/17.
+//  Copyright © 2017 Lisa Steele. All rights reserved.
+//
+
+import UIKit
+
+class Contact: NSObject {
+    var name: String?
+    var phoneNumber: String?
+    
+    init(name: String? = nil, phoneNumber: String? = nil) {
+        self.name = name
+        self.phoneNumber = phoneNumber
+        super.init()
+    }
+}
